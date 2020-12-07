39a315a8449aa274f3b1a851ec62376c85ec80ff
zhangxueren <zhangxueren@zuoyebang.com>
2020-12-07 11:35:30 +0800

    init

--- src/Supports/Functions.php
+++ src/Supports/Functions.php
@@ -1,13 +1,13 @@
 <?php
 
-use Gaia\Support\Debug\HtmlDumper;
 use Symfony\Component\VarDumper\Cloner\VarCloner;
 use Symfony\Component\VarDumper\Dumper\CliDumper;
+use Symfony\Component\VarDumper\Dumper\HtmlDumper;
 
 if (!function_exists('dd')) {
     function dd($value) {
         if (class_exists(CliDumper::class)) {
-            $dumper = 'cli' === PHP_SAPI ? new CliDumper : new HtmlDumper;
+            $dumper = 'cli' === PHP_SAPI ? new CliDumper : new HtmlDumper();
 
             $dumper->dump((new VarCloner)->cloneVar($value));
         } else {
---
+++ tests/OrmTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Zyb\Edison\Tests;
+
+use http\Exception\InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class OrmTest extends TestCase
+{
+    // 检查 $type 参数
+    public function testHelloWorld()
+    {
+        $this->assertTrue(true);
+    }
+}
